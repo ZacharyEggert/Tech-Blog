3fa4b969e3ced3ee145d8851a9bfdaf01a7894f8
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-05-17 19:34:46 -0700

    update models

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,2 @@
-drop database if exists tech-blog-db;
-
-create database if not exists tech-blog-db;
+drop database if exists tech_blog_db;
+create database if not exists tech_blog_db;
--- models/Post.js
+++ models/Post.js
@@ -1,33 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
-Post.init({
-    id:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        title: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT({ length: "long" }),
+            allowNull: false,
+        },
     },
-    user_id:{
-        type: DataTypes.INTEGER,
-        references:{
-            model:'user',
-            key:'id'
-        }
-    },
-    title:{
-        type: DataTypes.TEXT({length:'long'}),
-        allowNull: false,
-
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post",
     }
-},{
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post',
-})
+);
 
 module.exports = Post;
